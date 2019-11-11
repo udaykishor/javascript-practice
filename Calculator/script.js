14b53decc27ae9465a4290427645406792b1eef0
Uday Kishor <40660053+udaykishor@users.noreply.github.com>
2019-11-11 13:25:08 +0530

    update

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,6 +1,5 @@
 window.addEventListener("load", initEvent);
 var expression = "";
-var temp= "";
 function initEvent() {
   var numbers = document.querySelectorAll(".number");
   var operand = document.querySelectorAll(".operand");
@@ -16,30 +15,18 @@ function initEvent() {
 }
 function appendNumbers() {
   var num = event.srcElement.innerHTML;
-  temp = expression;
   expression += num;
   document.querySelector("#insert").value = expression;
 }
 function appendOperator() {
   var num = event.srcElement.innerHTML;
   expression += num;
-  temp
-  if (expression.charAt(expression.length - 1) == "+") {
-    expression = expression.substring(0, expression.length - 1);
+  if (expression.charAt(expression.length - 1) == '+'||expression.charAt(expression.length - 1) == '-'||expression.charAt(expression.length - 1) == '*'||expression.charAt(expression.length - 1) == '/') {
     document.querySelector("#insert").value = expression;
-  } else if (expression.charAt(expression.length - 1) == "-") {
     expression = expression.substring(0, expression.length - 1);
-    document.querySelector("#insert").value = expression;
-  } else if (expression.charAt(expression.length - 1) == "*") {
-    expression = expression.substring(0, expression.length - 1);
-    document.querySelector("#insert").value = expression;
-  } else if (expression.charAt(expression.length - 1) == "/") {
-    expression = expression.substring(0, expression.length - 1);
-    document.querySelector("#insert").value = expression;
   } else {
-    document.querySelector("#insert").value = expression;
+      
   }
-  
 }
 function calc() {
   var result = eval(expression);
